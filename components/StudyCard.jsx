485351a1549d82bcabd1f2cae72d2a26b4d8161d
Camilo Pinilla <camilo.pinilla2503@gmail.com>
2023-09-09 10:16:22 -0500

    feat: :sparkles: Creacion de componentes dos componentes reutilizables e implementacion de sus estilos
    Componente de carta de estudio y componente titulo creados e implementados

---
+++ components/StudyCard.jsx
@@ -0,0 +1,15 @@
+export default function StudyCard(props) {
+    return(
+        <article className=" p-4 h-1/3 relative border-b-2 border-black px-4 grid place-content-center  md:border-none">
+            <h3 className="p-4 flex text-lg md:text-3xl underline decoration-red-400 justify-between">
+                {props.title}
+            </h3>
+            <p className="text-base md:text-xl py-4 px-4">{props.desc}</p>
+            <div className="md:absolute inline-flex md:flex mb-2 mx-4 md:bottom-0 md:right-0">
+                <small className="ml-auto py-2 px-4 block  font-nixie font-semibold   ">{props.dataone}</small>
+                <small className="py-2 px-4 block   font-nixie font-semibold  ">{props.datatwo}</small>
+                <small className="py-2 px-4 block  font-nixie font-semibold  ">{props.datathree}</small>
+            </div>
+        </article>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import Scroll from "../components/SmoothScroll";
 
+import SectionTitle from "../components/SectionTitle";
+import Scroll from "../components/SmoothScroll";
+import StudyCard from "../components/StudyCard";
 
 export default function App() {
   return (
     <div>
+      {/* //TODO: header */}
       <header className="bg-blue-300 w-screen h-auto relative border border-transparent md:h-screen font-questrial text-2xl">
         <img src="paint.webp" className="absolute md:w-[480px] w-[150px] h-[150px] md:h-[453px] top-[-10px] left-[-10px]" alt="" />
         <div className="py-8 px-2 flex justify-end items-center">
@@ -25,6 +28,7 @@ export default function App() {
         </a>
       </header>
       <main className="font-questrial h-auto">
+        {/* //TODO: first section */}
         <section id="section-one" className="h-auto bg-red-400 py-20">
           <div className=" px-8 ">
             <p className=" md:w-[500px]  h-auto text-base md:text-xl 2xl:text-3xl">Bienvenido a mi portafolio, soy Camilo Pinilla, un apasionado
@@ -39,7 +43,8 @@ export default function App() {
               tendencias en UI/UX con el uso de tecnologias y estandares empleados en la industria.</p>
           </div>
         </section>
-        <section className="h-auto md:flex py-4 px-6 md:justify-between">
+        {/* //TODO: second section */}
+        <section className="h-auto md:flex py-4 mb-12 mt-4 px-6 md:justify-between">
           <div className="md:w-1/2 w-full h-auto grid place-content-center p-2 justify-center">
             <h2 className="text-2xl  md:text-7xl py-8">Perfil profesional</h2>
             <p className="text-base  md:text-xl leading-relaxed">Tecnologo en anàlisis y desarrollo de software, con aptitudes enfocadas a la investigacion, resolucion de problemas y necesidades, diseño de arquitecturas de sofware, desarrollo e implementacion de aplicaciones de software haciendo uso de diversas tecnologias.</p>
@@ -48,13 +53,50 @@ export default function App() {
             <br />
             <button className="text-base border border-black md:text-xl py-4 px-8 mx-auto mt-8 flex justify-center text-center md:w-[45%]"><a href="#">sobre camilo</a></button>
           </div>
-          <div className="h-auto md:w-1/2 p-4 md:shrink-0 relative flex items-center justify-center ">
+          <div className="h-auto md:w-1/2 py-4 px-2 md:shrink-0 relative flex items-  justify-center ">
             <div className="h-full w-full top-0 left-0 absolute hidden"></div>
-            <img src="photo-one.webp" alt="" className="h-[70%] mt-8 object-cover" />
+            <img src="photo-one.webp" alt="" className=" w-full sm:w-3/5 md:w-[70%]  mt-8 object-cover" />
+          </div>
+        </section>
+        {/* //TODO: third section */}
+        <section className="h-auto">
+          <SectionTitle title="Formacion academica" />
+          <div className="md:flex mt-8 h-auto">
+            <div className="md:w-2/6 hidden md:block">
+              <p className="text-[160px] border py-4 h-1/3 text-center">01</p>
+              <p className="text-[160px] border py-4 h-1/3 text-center">02</p>
+              <p className="text-[160px] border py-4 h-1/3 text-center">03</p>
+            </div>
+            <div className="md:w-2/3  h-auto">
+              <StudyCard
+                title="Analisis y desarrollo de sofware"
+                desc="Certificiacion en tecnologia de analisis y desarrollo de software, adquiriendo conocimiento de los procesos en cada fase del ciclo de desarrollo de software "
+                dataone="Servicio Nacional de Aprendizaje "
+                datatwo="Bogota - Colombia"
+                datathree="2022 - 2024"
+                
+              />
+              <StudyCard
+                title="Fundamentos de programacion"
+                desc="Curso de fundamentos de programacion, enfocado en el lenguaje JavaScript y ejecutando varios ejercicios consolidando el conocimiento adquirido. "
+                dataone="Fundacion telefonica Movistar"
+                datatwo="Bogota - Colombia"
+                datathree="16 OCT - 2021"
+                
+              />
+              <StudyCard
+                title="Implementacion de soluciones  de ciberseguridad en la nube "
+                desc="Bootcamp en implementacion de soluciones de ciberseguridad en la nube en la plataforma Microsoft Azure, desempenando labores de administracion de redes y la gestion de acceso."
+                dataone="Intelligent training"
+                datatwo="Bogota - Colombia"
+                datathree="23 SEPT - 2022"
+                
+              />
+            </div>
           </div>
         </section>
       </main>
-    <Scroll />
+      <Scroll />
     </div>
 
   )
