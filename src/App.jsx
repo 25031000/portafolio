162698bee48dc5964f4dda3262869daad107711c
Camilo Pinilla <camilo.pinilla2503@gmail.com>
2023-09-17 13:08:43 -0500

    feat: :lipstick: Mejora radical en la interfaz principal
    Modificacion completa de la estructura y diseno del portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,78 +24,73 @@ export default function App() {
       <Header />
       <main className="font-questrial h-auto border border-transparent  trigger text-tertiary">
         {/* //TODO: Presentation section */}
-        <section id="section-one" className="h-auto md:mt-56">
-          <div className=" px-4 md:px-8 flex flex-col">
+        <section id="section-one" className="h-auto md:mt-56 relative">
+          <div className=" px-4 md:px-8 flex flex-col ">
             <div className=" p-4  hidden md:flex justify-center mt-8 md:mt-0 items-center md:h-60">
-              <p className=" h-auto box text-7xl md:text-[160px]">UX/UI</p>
+              <p className=" h-auto box text-7xl md:text-[160px] box-shadow"><strong>UX/UI</strong></p>
             </div>
-            <div className=" p-4 md:p-8 border modal md:opacity-0 md:bg-primary gradient text-primary sm:border-none mt-8 rounded-xl w-full md:w-[50%]  flex justify-start">
-              <p className="text-base  md:p-4 md:w-[500px] md:text-3xl 2xl:text-6xl block ">
-                <strong>¡Bienvenid@ a mi portafolio web! Soy un desarrollador front-end  apasionado por crear experiencias de usuario impactantes. Explora mi trabajo en UX/UI.</strong></p>
-            </div>
-            <div className="h-auto w-full mt-4 md:mt-2 md:w-[50%] rounded-xl md:opacity-0 modalTwo p-1 md:p-2 gradient">
-              <div className=" p-2 md:p-8 h-full bg-primary text-tertiary rounded-md relative w-full  flex justify-start">
-                <p className="text-base  md:p-2 md:w-[500px] md:text-3xl 2xl:text-6xl block ">
-                  <strong>Te invito a explorar mis proyectos personales, donde plasmo mi pasión por la innovación y el diseño. Descubre mi creatividad y enfoque en cada uno de ellos. ¡Bienvenid@!</strong></p>
-              </div>
-            </div>
-
           </div>
+          <div className=" md:p-8 md:ml-12 sm:border hidden md:flex modal md:opacity-0 text-transparent bg-clip-text text-grad  sm:border-none mt-8 mx-auto md:w-[51%] h-auto  justify-start">
+            <p className=" md:p-4 w-full md:text-[52px] leading-tight 2xl:text-6xl block ">
+              <strong>¡Bienvenid@ a mi portafolio web! Soy un desarrollador front-end  apasionado por crear experiencias de usuario impactantes. Explora mi trabajo en UX/UI.</strong></p>
+          </div>
+          <img src="photo-one.webp" className="absolute right-32 img-ani opacity-0 w-[350px]  h-[620px]" alt="" srcset="" />
+          <p className="md:hidden block bg-gradient-to-r sm:text-center text-grad text-3xl p-4 pl-5 bg-clip-text text-transparent"><strong>¡Bienvenid@ a mi portafolio web!</strong></p>
         </section>
         {/* //TODO: Professional profile section */}
-        <section className="h-auto md:flex py-4 mb-12 mt-4 px-6 md:justify-between">
-          <div className="md:w-1/2 w-full h-auto grid place-content-center p-2 justify-center">
-            <h2 className="text-2xl  md:text-7xl py-8">Perfil profesional</h2>
-            <p className="text-base  md:text-xl leading-relaxed">Tecnologo en anàlisis y desarrollo de software, con aptitudes investigativas, resolucion de problemas y necesidades, diseño de arquitecturas de sofware, desarrollo e implementacion de aplicaciones de software haciendo uso de diversas tecnologias.</p>
-            <br />
-            <p className="text-base md:text-xl leading-relaxed">Experiencia solida en el campo del desarrollo Front-end aplicando practicas de experiencia de usuario y usabilidad , prototipado de interfaces de usuario, Implementando diseños responsivos adaptables a diferentes tamaños de pantallas.</p>
-            <br />
-            <div>
-              <button className="text-base btn-shadow bg-tertiary text-primary md:text-xl py-4 px-8 mx-auto mt-8 flex justify-center text-center md:w-[45%]"><a href="#">sobre camilo</a></button>
+        <section className="h-auto grid border  md:px-12 md:grid-cols-2 gap-2 p-4 mb-12 ">
+          <card className=" w-full h-auto p-8 md:py-12 pl-8 flex flex-col justify-center rounded-3xl border bg-tertiary text-primary">
+            <h2 className="text-2xl  md:text-7xl mb-8 "><strong>Perfil profesional</strong></h2>
+            <p className="md:text-2xl leading-relaxed">
+              <strong className="md:block hidden">Tecnologo en anàlisis y desarrollo de software, con aptitudes investigativas, resolucion de problemas, diseño de arquitecturas de sofware, desarrollo de aplicaciones de software.</strong>
+              <span className="md:hidden block">Tecnologo en anàlisis y desarrollo de software, con aptitudes investigativas, resolucion de problemas, diseño de arquitecturas de sofware, desarrollo de aplicaciones de software.</span>
+            </p>
+          </card>
+          <card className="h-auto gap-2 flex flex-col">
+            <div className=" gradient md:py-8 text-primary rounded-3xl h-auto p-4 md:flex ">
+              <div className=" flex w-full flex-col p-4 justify-center items-center">
+                <p className="text-3xl  w-full">
+                  <strong >Analisis y desarrollo de software</strong>
+                </p>
+                <p className="py-4 md:text-xl">
+                  <strong className="md:block hidden">Certificado en Analisis y desarrollo de software, con experiencia en cada fase del desarrollo de software</strong>
+                  <span className="md:hidden block">Certificado en Analisis y desarrollo de software, con experiencia en cada fase del desarrollo de software</span>
+                </p>
+              </div>
+
             </div>
-
-          </div>
-          <div className="h-auto md:w-1/2 py-4 px-2 md:shrink-0 relative flex items-  justify-center ">
-            <div className="h-full w-full top-0 left-0 absolute hidden"></div>
-            <img src="photo-one.webp" alt="" className=" w-full sm:w-3/5 md:w-[70%]  mt-8 object-cover" />
-          </div>
-        </section>
-        {/* //TODO: Education section */}
-        <section className="h-auto">
-          <SectionTitle title="Formacion academica" />
-          <div className="md:flex mt-8 h-auto">
-            <div className="md:w-2/6 hidden md:block">
-              <p className="text-[160px]  py-4 h-1/3 text-center">01</p>
-              <p className="text-[160px]  py-4 h-1/3 text-center">02</p>
-              <p className="text-[160px]  py-4 h-1/3 text-center">03</p>
+            <div className="border rounded-3xl md:py-8 h-auto p-4 md:flex ">
+              <div className=" flex w-full flex-col p-4 justify-center items-center">
+                <p className="text-3xl  w-full">
+                  <strong >Fundamentos de programacion</strong>
+                </p>
+                <p className=" py-4 md:text-xl">
+                  <strong className="md:block hidden">Curso en fundamentos de programacion con lenguaje JavaScript.</strong>
+                  <span className="md:hidden block">Curso en fundamentos de programacion con lenguaje JavaScript.</span>
+                </p>
+              </div>
             </div>
-            <div className="md:w-2/3  h-auto">
-              <StudyCard
-                title="Analisis y desarrollo de sofware"
-                desc="Certificiacion en tecnologia de analisis y desarrollo de software, adquiriendo conocimiento de los procesos en cada fase del ciclo de desarrollo de software "
-                dataone="Servicio Nacional de Aprendizaje "
-                datatwo="Bogota - Colombia"
-                datathree="2022 - 2024"
-
-              />
-              <StudyCard
-                title="Fundamentos de programacion"
-                desc="Curso de fundamentos de programacion, enfocado en el lenguaje JavaScript y ejecutando varios ejercicios consolidando el conocimiento adquirido. "
-                dataone="Fundacion telefonica Movistar"
-                datatwo="Bogota - Colombia"
-                datathree="16 OCT - 2021"
-
-              />
-              <StudyCard
-                title="Implementacion de soluciones  de ciberseguridad en la nube "
-                desc="Bootcamp en implementacion de soluciones de ciberseguridad en la nube en la plataforma Microsoft Azure, desempenando labores de administracion de redes y la gestion de acceso."
-                dataone="Intelligent training"
-                datatwo="Bogota - Colombia"
-                datathree="23 SEPT - 2022"
-
-              />
+          </card>
+          <card className=" h-auto w-full gap-2 sm:flex justify-center ">
+            <div className=" h-auto sm:w-1/2 p-8 pl-8 flex justify-center items-center rounded-3xl border">
+              <p className="text-5xl"><strong>Git</strong></p>
+              <img src="git.webp" className="w-12 h-12" alt="" />
             </div>
-          </div>
+            <div className=" h-auto sm:w-1/2 p-8 pl-8 md:py-12 flex  justify-center items-center rounded-3xl border">
+              <p className="text-5xl"><strong>Tailwind</strong></p>
+              <img src="tailwind.webp" className="w-12 h-12" alt="" />
+            </div>
+          </card>
+          <card className=" h-auto w-full gap-2 sm:flex justify-center ">
+            <div className=" h-auto sm:w-1/2 p-8 pl-8 flex justify-center rounded-3xl items-center border">
+              <p className="text-5xl"><strong>React</strong></p>
+              <img src="react.webp" className="w-12 h-12" alt="" />
+            </div>
+            <div className=" h-auto sm:w-1/2 p-8 pl-8 md:py-12 flex  justify-center items-center rounded-3xl border">
+              <p className="text-5xl"><strong>Sqlite</strong></p>
+              <img src="sq.svg" className="w-12 h-12" alt="" />
+            </div>
+          </card>
         </section>
         {/* //TODO: Technologies section */}
         <section className="h-auto mt-12 md:mt-48">
